750b0781d311260571ceb00054536b9ece912615
thisni1s <nils@jn2p.de>
2024-01-04 00:36:57 +0100

    better name subcommand

--- cmd/name.go
+++ cmd/name.go
@@ -16,29 +16,42 @@ var nameCmd = &cobra.Command{
 	Use:   "name",
 	Short: "Search for Members by Name",
 	Long: `Returns all Members whos name match the specified name.
-Both fName and lName are optional, but one is required
+This command supports two arguments, if one argument is given nami-cli will search for first and last names mathing this argument.
+If two arguments are given, it will search for a Member matching both arguments
 
-Example:
-  nami-cli search name --fName John --lName Doe`,
+Examples:
+  nami-cli search name John
+  nami-cli search name Doe
+  nami-cli search name John Doe`,
+	Args: cobra.RangeArgs(1, 2),
 	Run: func(cmd *cobra.Command, args []string) {
-        if *fName == "" && *lName == "" {
-            log.Fatal("At least one Flag is required!")
-        }
-		Login()
 		var searchCfg namiTypes.SearchValues
-		if *fName != "" {
-			searchCfg.Vorname = *fName
+		Login()
+		if len(args) == 1 {
+			searchCfg.Vorname = args[0]
+			list, err := namigo.Search(searchCfg)
+			if err != nil {
+				log.Println("Failed to get Members for provided first name!")
+				log.Fatal(err)
+			}
+			searchCfg.Vorname = ""
+			searchCfg.Nachname = args[0]
+			list2, err := namigo.Search(searchCfg)
+			if err != nil {
+				log.Println("Failed to get Members for provided last name!")
+				log.Fatal(err)
+			}
+			PrintSearchResult(append(list, list2...))
+		} else if len(args) == 2 {
+            searchCfg.Vorname = args[0]
+            searchCfg.Nachname = args[1]
+			list, err := namigo.Search(searchCfg)
+			if err != nil {
+				log.Println("Failed to get Members for provided Name!")
+				log.Fatal(err)
+			}
+			PrintSearchResult(list)
 		}
-		if *lName != "" {
-			searchCfg.Nachname = *lName
-		}
-		list, err := namigo.Search(searchCfg)
-		if err != nil {
-			log.Println("Failed to get Members for provided Name!")
-			log.Fatal(err)
-		}
-		PrintSearchResult(list)
-
 	},
 }
 
@@ -56,7 +69,7 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	fName = nameCmd.Flags().StringP("fName", "v", "", "First name (if any)")
-	lName = nameCmd.Flags().StringP("lName", "l", "", "Last name (if any)")
-    nameCmd.MarkFlagsOneRequired("fName", "lName")
+	//fName = nameCmd.Flags().StringP("fName", "v", "", "First name (if any)")
+	//lName = nameCmd.Flags().StringP("lName", "l", "", "Last name (if any)")
+	//nameCmd.MarkFlagsOneRequired("fName", "lName")
 }
